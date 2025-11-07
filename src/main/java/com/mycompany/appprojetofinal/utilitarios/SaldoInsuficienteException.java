2f810d35ce105552a36a835f16a4222be3aef496
vitor <vitoremmanuel04@icloud.com>
2025-11-07 20:24:12 -0300

    projetor

--- src/main/java/com/mycompany/appprojetofinal/utilitarios/SaldoInsuficienteException.java
+++ src/main/java/com/mycompany/appprojetofinal/utilitarios/SaldoInsuficienteException.java
@@ -5,6 +5,7 @@ public class SaldoInsuficienteException extends Exception {
     public SaldoInsuficienteException() {
         super("Saldo insuficiente");
     }
+
     public SaldoInsuficienteException(String s) {
         super(s);
     }
